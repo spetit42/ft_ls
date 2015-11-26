5fcb275d01a373b2fa3ab3678c7d808b1262a812
Sonny PETIT <spetit@e2r7p14.42.fr>
2015-11-26 20:19:37 +0100

    up

--- ft_ls.h
+++ ft_ls.h
@@ -21,21 +21,4 @@
 void    sort_list(t_list *list);
 void	ft_print_list(t_list *list);
 
-// typedef struct	s_options
-// {
-// 	unsigned char	l_opt;
-// 	unsigned char	R_opt;
-// 	unsigned char	a_opt;
-// 	unsigned char	r_opt;
-// 	unsigned char	t_opt;
-// }				t_options;
-
-// typedef struct	s_log
-// {
-// 	char	*name;
-// 	char	*permissions;
-// 	time_t	last_modification;
-// }				t_log;
-
-
 #endif
